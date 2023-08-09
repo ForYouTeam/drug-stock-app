995888be59204a3fb99ea5157b1a269dd49fc88c
Fahrezy22 <fahriza2222@gmail.com>
2023-08-09 22:43:13 +0700

    make auth func

--- resources/views/Auth/Login.blade.php
+++ resources/views/Auth/Login.blade.php
@@ -17,25 +17,23 @@
                     <h1 class="auth-title">Log in.</h1>
                     <p class="auth-subtitle mb-5">Silahkan login untuk masuk kedalam dashboard.</p>
 
-                    <form action="">
+                    <form action="{{ route('login.process') }}" method="POST">
+                        @csrf
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" placeholder="Username">
+                            <input type="text" class="form-control form-control-xl" name="username" placeholder="Username">
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                             </div>
                         </div>
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <input type="password" name="password" class="form-control form-control-xl" placeholder="Password">
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                             </div>
                         </div>
-                        <div class="form-check form-check-lg d-flex align-items-end">
-                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                                Keep me logged in
-                            </label>
-                        </div>
+                        @if ($errors->has('email'))
+                            <span class="text-danger text-left">{{ $errors->first('email') }}</span>
+                        @endif
                         <button class="btn btn-primary btn-block btn-lg shadow-lg mt-4">Log in</button>
                     </form>
                 </div>
